6f1a74d756d471070fd3a368b6ef969c38508970
joancastro <castrojoan58@live.com>
2022-04-11 01:16:18 -0500

    added routes for home, search, and admin

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -27,11 +27,11 @@ const NavigationBar = () => {
                             navbarScroll
                         >
                             <NavDropdown title="More" id="collapsible-nav-dropdown"  className="ms-auto">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Item href="/">Home</NavDropdown.Item>
+                                <NavDropdown.Item href="/search">Search</NavDropdown.Item>
+
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                <NavDropdown.Item href="/admin">Administrator Tools</NavDropdown.Item>
                             </NavDropdown>
                             <Nav.Item className="nav-button">
                                 <Button variant="outline-success">Sign in</Button>
